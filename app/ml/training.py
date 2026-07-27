7e1cb5071da099a65485730eca4a9e7999d66a6b
sadeclipse <nedoviziy2004@gmail.com>
2026-07-27 19:13:05 +0300

    wip

--- app/ml/training.py
+++ app/ml/training.py
@@ -1,16 +1,21 @@
 from sklearn.model_selection import train_test_split
-
+from sklearn.ensemble import RandomForestClassifier
 from data.dataset_loading import get_dataframe
 
-def get_features_and_target():
+def load_and_train():
     df = get_dataframe("app/data/reWine.csv") 
-    features = df.iloc[:,1:]
-    target = df.iloc[:, 0]
+    features = df.iloc[:,0:-1]
+    target = df.iloc[:, -1]
     train_X, test_X, train_y, test_y = train_test_split(features, target, test_size=0.2)
+    RF = RandomForestClassifier(n_estimators=400
+                                )
+    RF.fit(X=train_X, y=train_y)
+    return RF
+    
 
 def main():
-    get_features_and_target()
-
+    model = load_and_train()    
+    print(model)
     
 if __name__ == "__main__":
     main()
