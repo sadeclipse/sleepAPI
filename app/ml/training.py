f83b4ef9964e0f4508987aebb8114d972ff9f720
sadeclipse <nedoviziy2004@gmail.com>
2026-08-07 00:22:29 +0300

    train a model and save it

--- app/ml/training.py
+++ app/ml/training.py
@@ -1,45 +1,106 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import *
+import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras import layers, models
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+CAT_COLS = ["gender", "physical_activity_level", "diet_type"]
+NUM_COLS = [
+    "age",
+    "bmi",
+    "caffeine_intake_mg",
+    "water_intake_liters",
+    "screen_time_hours",
+    "daily_steps",
+    "calories_burned",
+    "resting_heart_rate",
+    "daily_sleep_hours",
+    "deep_sleep_hours",
+]
+OUT_CLASSES_NUM = 3
 
 
-def load_df(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path)
-    return df
+def get_train_test(df, cat_cols, num_cols):
+    target_cat = df.iloc[:, -1].values
+    target_num = df.iloc[:, -2].values
 
+    if target_cat.dtype == object or target_cat.dtype.name == "category":
+        le = LabelEncoder()
+        target_cat = le.fit_transform(target_cat)
 
-def get_train_test(df: pd.DataFrame):
-    features = df.iloc[:, 0:-2]
-    target_cat = df.iloc[:, -1]
-    target_num = df.iloc[:, -2]
-    return train_test_split(
-        features,
-        target_cat,
-        target_num,
-        test_size=0.2,
-        random_state=69,
+    target_num = target_num.astype(np.float32)
+
+    features = df.drop(columns=[df.columns[-1], df.columns[-2]])
+
+    X_train, X_test, y_cat_train, y_cat_test, y_num_train, y_num_test = (
+        train_test_split(
+            features, target_cat, target_num, test_size=0.2, random_state=69
+        )
+    )
+
+    def to_inputs_dict(X_data):
+        d = {}
+        for col in num_cols:
+            d[col] = X_data[col].values
+        for col in cat_cols:
+            d[col] = X_data[col].astype(str).values
+        return d
+
+    return (
+        to_inputs_dict(X_train),
+        to_inputs_dict(X_test),
+        y_cat_train,
+        y_cat_test,
+        y_num_train,
+        y_num_test,
     )
 
 
-def build_nn(input_dim, out_classes_num) -> models.Model:
-    inputs = tf.keras.Input(shape=(input_dim,))
-    x = layers.Dense(32, activation="relu")(inputs)
+def build_nn(X_train_dict, cat_cols, num_cols, out_classes_num) -> models.Model:
+    all_inputs = {}
+    encoded_features = []
+
+    for col in num_cols:
+        all_inputs[col] = layers.Input(shape=(1,), name=col, dtype=tf.float32)
+
+        numeric_normalizer = layers.Normalization(axis=None)
+        numeric_normalizer.adapt(X_train_dict[col])
+
+        encoded_features.append(numeric_normalizer(all_inputs[col]))
+
+    for col in cat_cols:
+        all_inputs[col] = layers.Input(shape=(1,), name=col, dtype=tf.string)
+
+        lookup = layers.StringLookup(output_mode="int")
+        lookup.adapt(X_train_dict[col])
+
+        num_tokens = lookup.vocabulary_size()
+        encoding = layers.CategoryEncoding(num_tokens=num_tokens, output_mode="one_hot")
+
+        col_encoded = encoding(lookup(all_inputs[col]))
+        encoded_features.append(col_encoded)
+
+    all_features = layers.concatenate(encoded_features)
+
+    x = layers.Dense(32, activation="relu")(all_features)
     x = layers.Dense(64, activation="relu")(x)
+    x = layers.Dropout(0.2)(x)
     x = layers.Dense(128, activation="relu")(x)
+    x = layers.Dropout(0.2)(x)
+
     classification_out = layers.Dense(
         out_classes_num, activation="softmax", name="classification_out"
     )(x)
+
     numerical_out = layers.Dense(1, activation="linear", name="numerical_out")(x)
 
-    model = models.Model(inputs=inputs, outputs=[classification_out, numerical_out])
+    model = models.Model(inputs=all_inputs, outputs=[classification_out, numerical_out])
+
     model.compile(
         optimizer="adam",
         loss={
-            "classification_out": "sparse_categorical_crossemtropy",
+            "classification_out": "sparse_categorical_crossentropy",
             "numerical_out": "mse",
         },
         metrics={"classification_out": "accuracy", "numerical_out": "mae"},
@@ -47,53 +108,25 @@ def build_nn(input_dim, out_classes_num) -> models.Model:
     return model
 
 
-def train_model(
-    model: models.Model,
-    train_X,
-    test_X,
-    train_cat_y,
-    test_cat_y,
-    train_num_y,
-    test_num_y,
-    epochs=20,
-):
-    history = model.fit(
-        x=train_X,
-        y={"classification_out": train_cat_y, "numerical_out": train_num_y},
-        validation_data=(
-            test_X,
-            {"classification_out": test_cat_y, "numerical_out": test_num_y},
-        ),
-        epochs=epochs,
-        batch_size=32,
-    )
-    return history
-
-
-def main():
-    df = load_df("C:/Users/user/Desktop/data/sleepapi/app/data/reSS.csv")
-    (
-        train_X,
-        test_X,
-        train_cat_y,
-        test_cat_y,
-        train_num_y,
-        test_num_y,
-    ) = get_train_test(df)
-    inp_shape = train_X.shape
-    model = build_nn(input_dim=inp_shape[1], out_classes_num=3)
-    history = train_model(
-        model,
-        train_X,
-        test_X,
-        train_cat_y,
-        test_cat_y,
-        train_num_y,
-        test_num_y,
-    )
-
-    print(history)
-
-
 if __name__ == "__main__":
-    main()
+    df = pd.read_csv("C:/Users/user/Desktop/data/sleepapi/app/data/reSS.csv")
+
+    X_train_dict, X_test_dict, y_cat_train, y_cat_test, y_num_train, y_num_test = (
+        get_train_test(df, CAT_COLS, NUM_COLS)
+    )
+
+    model = build_nn(X_train_dict, CAT_COLS, NUM_COLS, out_classes_num=OUT_CLASSES_NUM)
+
+    model.fit(
+        x=X_train_dict,
+        y={"classification_out": y_cat_train, "numerical_out": y_num_train},
+        epochs=40,
+        batch_size=32,
+        validation_data=(
+            X_test_dict,
+            {"classification_out": y_cat_test, "numerical_out": y_num_test},
+        ),
+    )
+
+    model.save("model.keras")
+    print("Модель успешно сохранена!")
--- app/schemas.py
+++ app/schemas.py
@@ -1,19 +1,44 @@
-from pydantic import BaseModel, Field
+from typing import Literal
+from pydantic import BaseModel, Field, NonNegativeInt, NonNegativeFloat
 
 
-class PredictionRequest(BaseModel):
-    fixed_acidity: float = Field(..., ge=0)
-    volatile_acidity: float = Field(..., ge=0)
-    citric_acid: float = Field(..., ge=0)
-    residual_sugar: float = Field(..., ge=0)
-    chlorides: float = Field(..., ge=0)
-    free_sulfur_dioxide: float = Field(..., ge=0)
-    total_sulfur_dioxide: float = Field(..., ge=0)
-    density: float = Field(..., ge=0)
-    pH: float = Field(..., ge=0)
-    sulphates: float = Field(..., ge=0)
-    alcohol: float = Field(..., ge=0)
+class HealthMetrics(BaseModel):
+    age: int = Field(..., gt=0, le=120, description="Возраст в годах")
+    gender: Literal["male", "female", "other"] = Field(..., description="Пол")
+    physical_activity_level: Literal["Medium", "Low", "High"] = Field(
+        ..., description="Уровень физической активности"
+    )
+    diet_type: Literal["Average", "Healthy", "Unhealthy"] = Field(
+        ..., description="Тип питания"
+    )
+
+    bmi: float = Field(..., gt=10.0, lt=60.0, description="Индекс массы тела")
+    caffeine_intake_mg: NonNegativeInt = Field(
+        ..., description="Потребление кофеина в мг"
+    )
+    water_intake_liters: NonNegativeFloat = Field(
+        ..., description="Потребление воды в литрах"
+    )
+    screen_time_hours: float = Field(
+        ..., ge=0.0, le=24.0, description="Экранное время в часах"
+    )
+    daily_steps: NonNegativeInt = Field(..., description="Количество шагов за день")
+    calories_burned: NonNegativeInt = Field(..., description="Сожженные калории (ккал)")
+    resting_heart_rate: int = Field(
+        ..., ge=10, le=150, description="Пульс в покое (уд/мин)"
+    )
+    daily_sleep_hours: float = Field(
+        ..., ge=0.0, le=24.0, description="Всего часов сна за сутки"
+    )
+    deep_sleep_hours: float = Field(
+        ..., ge=0.0, le=24.0, description="Часов глубокого сна"
+    )
 
 
 class PredictionResponse(BaseModel):
-    quality: int = Field(..., ge=0, le=10)
+    sleep_quality_score: float = Field(
+        ..., gt=0.0, lt=10.0, description="Оценка качества сна"
+    )
+    daily_stress_level: Literal["Low", "Medium", "High"] = Field(
+        ..., description="Уровень стресса в течение дна"
+    )
