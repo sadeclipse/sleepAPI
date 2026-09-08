3786120dc071d1c2ce6ae896c606a1bff75b31ac
sadeclipse <nedoviziy2004@gmail.com>
2026-09-08 17:55:00 +0300

    change db to postgres

--- app/database.py
+++ app/database.py
@@ -4,9 +4,7 @@ from app.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args=(
-        {"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {}
-    ),
+    pool_size=5
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
